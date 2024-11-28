c7f8e445bfa59ff4199531eab13d0214011ae811
Emelie Hansson <emeliehanssonn@hotmail.se>
2024-11-28 14:51:21 +0100

    mapview working

---
+++ app.config.ts
@@ -0,0 +1,33 @@
+import * as dotenv from "dotenv";
+
+dotenv.config();
+
+export default {
+	expo: {
+		name: "Furbook",
+		slug: "furbook",
+		extra: {
+			googleMapsApiKey:
+				process.env.GOOGLE_MAPS_API_KEY || "default-api-key",
+			eas: {
+				projectId: "3d270750-84ce-4f83-b00d-83fb00d0e0f7",
+			},
+		},
+		android: {
+			config: {
+				googleMaps: {
+					apiKey:
+						process.env.GOOGLE_MAPS_API_KEY || "default-api-key",
+				},
+			},
+		},
+		ios: {
+			config: {
+				googleMaps: {
+					apiKey:
+						process.env.GOOGLE_MAPS_API_KEY || "default-api-key",
+				},
+			},
+		},
+	},
+};
