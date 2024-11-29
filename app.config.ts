3952e125c98e1c9b6fbce7e4a14282d838e0d7e5
Emelie Hansson <emeliehanssonn@hotmail.se>
2024-11-29 22:59:13 +0100

    stuff

--- app.config.ts
+++ app.config.ts
@@ -6,22 +6,20 @@ export default {
 	expo: {
 		name: "Furbook",
 		slug: "furbook",
-		extra: {
-			googleMapsApiKey:
-				process.env.GOOGLE_MAPS_API_KEY || "default-api-key",
-			eas: {
-				owner: "misspixxie",
-				projectId: "3d270750-84ce-4f83-b00d-83fb00d0e0f7",
-			},
-		},
 		android: {
 			config: {
 				googleMaps: {
-					apiKey:
-						process.env.GOOGLE_MAPS_API_KEY || "default-api-key",
+					apiKey: process.env.GOOGLE_MAPS_API_KEY,
 				},
 			},
 			package: "com.misspixxie.Furbook",
+		},
+		extra: {
+			owner: "misspixxie",
+			googleMapsApiKey: process.env.GOOGLE_MAPS_API_KEY,
+			eas: {
+				projectId: "3d270750-84ce-4f83-b00d-83fb00d0e0f7",
+			},
 		},
 	},
 };
