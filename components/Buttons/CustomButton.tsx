ab22093c71425f7848a9bab4e24a171078f592d5
Emelie Hansson <emeliehanssonn@hotmail.se>
2024-12-04 23:01:11 +0100

    Fix line endings

--- components/Buttons/CustomButton.tsx
+++ components/Buttons/CustomButton.tsx
@@ -1,72 +1,73 @@
+import { LinearGradient } from "expo-linear-gradient";
 import React from "react";
 import {
-	Text,
-	StyleSheet,
-	TouchableOpacity,
-	DimensionValue,
+  Text,
+  StyleSheet,
+  TouchableOpacity,
+  DimensionValue,
 } from "react-native";
 
 interface Props {
-	title: string;
-	onPress: () => void;
-	bgColor?: any;
-	borderColor?: string;
-	color?: string;
-	borderWidth?: number;
-	fontSize?: FontSize;
-	icon?: any;
-	marginBottom?: number;
-	width?: number | DimensionValue;
-	padding?: number;
-	children?: React.ReactNode;
+  title: string;
+  onPress: () => void;
+  bgColor?: any;
+  borderColor?: string;
+  color?: string;
+  borderWidth?: number;
+  fontSize?: FontSize;
+  icon?: any;
+  marginBottom?: number;
+  width?: number | DimensionValue;
+  padding?: number;
+  children?: React.ReactNode;
 }
 
 type FontSize = 16 | 18 | 20 | 22 | 26 | 28 | 32;
 
 export const CustomButton = ({
-	title,
-	onPress,
-	bgColor,
-	borderColor,
-	color,
-	borderWidth,
-	fontSize = 22,
-	icon,
-	marginBottom,
-	width,
-	padding,
-	children,
+  title,
+  onPress,
+  bgColor,
+  borderColor,
+  color,
+  borderWidth,
+  fontSize = 22,
+  icon,
+  marginBottom,
+  width,
+  padding,
+  children,
 }: Props) => {
-	const styles = StyleSheet.create({
-		button: {
-			width: width,
-			backgroundColor: bgColor,
-			borderWidth: borderWidth,
-			borderRadius: 10,
-			marginVertical: 10,
-			alignSelf: "center",
-			shadowColor: "#171717",
-			shadowOffset: { width: -2, height: 4 },
-			shadowOpacity: 0.2,
-			shadowRadius: 3,
-			elevation: 3,
-			borderColor: borderColor,
-			marginBottom: marginBottom,
-			padding: padding,
-		},
-		text: {
-			fontSize: fontSize,
-			color: color,
-			paddingHorizontal: 5,
-			paddingVertical: 10,
-			textAlign: "center",
-		},
-	});
+  const styles = StyleSheet.create({
+    button: {
+      width: width,
+      backgroundColor: bgColor,
+      borderWidth: borderWidth,
+      borderRadius: 10,
+      marginVertical: 10,
+      alignSelf: "center",
+      shadowColor: "#171717",
+      shadowOffset: { width: -2, height: 4 },
+      shadowOpacity: 0.2,
+      shadowRadius: 3,
+      elevation: 3,
+      borderColor: borderColor,
+      marginBottom: marginBottom,
+      padding: padding,
+    },
+    text: {
+      fontSize: fontSize,
+      color: color,
+      paddingHorizontal: 5,
+      paddingVertical: 10,
+      textAlign: "center",
+    },
+  });
 
-	return (
-		<TouchableOpacity style={styles.button} onPress={onPress}>
-			{children}
-			<Text style={styles.text}>{title}</Text>
-		</TouchableOpacity>
-	);
+  return (
+    <TouchableOpacity style={styles.button} onPress={onPress}>
+      {children}
+      <Text style={styles.text}>{title}</Text>
+    </TouchableOpacity>
+  );
 };
